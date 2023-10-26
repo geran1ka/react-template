2b9c740a2ed1e1e697645cb6b8c6eb9c50aa5f25
Geran1ka <roman.khoruzhy@gmail.com>
2023-10-26 16:40:32 +0300

    Сборка react-webpack

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import img from './img/about-us-two.jpg';
+
+const Module = () => (
+  <>
+    <h1>Hello, React!</h1>
+    <img width={200} src={img} />
+  </>
+);
+
+export default Module;
